9bedf24006efb7892d7b10a5b6e8f3ea948acfd9
Mateus Vagner <mateus.vagner@gmail.com>
2023-04-10 20:50:04 -0300

    refactor: add database instance

--- app/src/main/java/com/ifrs/coffeehouse/data/CoffeeHouseAppDatabase.kt
+++ app/src/main/java/com/ifrs/coffeehouse/data/CoffeeHouseAppDatabase.kt
@@ -1,9 +1,12 @@
 package com.ifrs.coffeehouse.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 import com.ifrs.coffeehouse.data.dao.CoffeeDao
 import com.ifrs.coffeehouse.data.entity.CoffeeEntity
+
 
 @Database(
     entities = [CoffeeEntity::class],
@@ -11,6 +14,18 @@ import com.ifrs.coffeehouse.data.entity.CoffeeEntity
 )
 abstract class CoffeeHouseAppDatabase : RoomDatabase() {
 
+    private var appDatabase: CoffeeHouseAppDatabase? = null
+
     abstract fun coffeeDao(): CoffeeDao
 
+    open fun getInstance(context: Context): CoffeeHouseAppDatabase? {
+        if (appDatabase == null) {
+            appDatabase = databaseBuilder(
+                context.applicationContext,
+                CoffeeHouseAppDatabase::class.java,
+                "database_name"
+            ).allowMainThreadQueries().build()
+        }
+        return appDatabase
+    }
 }
